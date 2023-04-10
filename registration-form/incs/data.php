bfd119d406927fd79dcd8815870197102a100b4b
Alyona1619 <108504553+Alyona1619@users.noreply.github.com>
2023-04-10 19:19:27 +0800

    Create data.php

---
+++ registration-form/incs/data.php
@@ -0,0 +1,32 @@
+<?php
+
+$fields = [
+    'name' => [
+        'field_name' => 'Имя',
+        'required' => 1,
+    ],
+    'surname' => [
+        'field_name' => 'Фамилия',
+        'required' => 1,
+    ],
+    'email' => [
+        'field_name' => 'Email',
+        'required' => 1,
+    ],
+    'phone' => [
+        'field_name' => 'Телефон',
+        'required' => 1,
+    ],
+    'topic' => [
+        'field_name' => 'Тематика конференции',
+        'required' => 1,
+    ],
+    'payment' => [
+        'field_name' => 'Способ оплаты',
+        'required' => 1,
+    ],
+    'agree' => [
+        'field_name' => 'Соглашение с рассылкой о конференции',
+        'required' => 0,
+    ],
+];
